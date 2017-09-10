bf7d7178d2cf19fff16245512c76a287e85f6a11
rafalpieniazek <rkpieniazek@gmail.com>
2017-09-10 12:02:07 +0200

    mocked library list

--- src/app/books/books-list/books-list.component.ts
+++ src/app/books/books-list/books-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'books-list',
@@ -7,7 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksListComponent implements OnInit {
 
-  constructor() { }
+  books = [
+    {
+      "id": 1,
+      "version": 0,
+      "genre": "CRIME",
+      "year": 2003,
+      "title": "Nie mów nikomu",
+      "author": "Coben"
+    },
+    {
+      "id": 2,
+      "version": 0,
+      "genre": "CRIME",
+      "year": 2006,
+      "title": "Wszyscy mamy tajemnice",
+      "author": "Coben"
+    },
+    {
+      "id": 3,
+      "version": 0,
+      "genre": "IT",
+      "year": 2004,
+      "title": "Clean Code",
+      "author": "Knuth"
+    },
+    {
+      "id": 4,
+      "version": 0,
+      "genre": "IT",
+      "year": 1992,
+      "title": "Sztuka programowania",
+      "author": "Martin"
+    }
+  ];
+
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
